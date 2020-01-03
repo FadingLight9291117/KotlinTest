a316d4964d54a96a73bd8537482cb686fa412646
L.Z.Che <FadingLight9291117@126.com>
2020-01-03 17:01:01 +0800

    ubuntu

--- src/lambdaAndCollection/Sequence.kt
+++ src/lambdaAndCollection/Sequence.kt
@@ -5,5 +5,11 @@ package lambdaAndCollection
 fun main(args: Array<String>) {
     val list = listOf(2, 2, 12, 4, 44)
     val list2 = list.asSequence().filter { it > 4 }.map { it*it }.toList()
-    list2.forEach { print("$it ") }
+    println(list2)
+
+    //
+    val natureNumList = generateSequence(0) { it + 1 }
+    val list3 = natureNumList.takeWhile { it <= 9 }.toList()
+    println(list3)
+
 }
